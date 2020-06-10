bdcf4136f8db9f8b98d299cee4875a84c198d790
Dinh <dinhhuynh21@gmail.com>
2020-06-10 12:15:35 +0700

    debug = true

--- AreaEnglish/settings.py
+++ AreaEnglish/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7)0b=6tc%w0(-t5b^3fm6$9t2omq+%^e07=&1dttgo-mqc6@2e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
