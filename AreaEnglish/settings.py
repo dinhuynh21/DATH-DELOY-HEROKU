b6b48cfd69613657cde0060bb1d0df9edc060912
dinhuynh21 <dinhhuynh21@gmail.com>
2020-06-25 15:54:11 +0700

    add static_root

--- AreaEnglish/settings.py
+++ AreaEnglish/settings.py
@@ -122,7 +122,7 @@ EMAIL_USE_SSL = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
